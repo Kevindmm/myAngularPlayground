6b60294faaff9c170d0ef0fa274e47507e8d5123
Kevin Montaño <kevindmm@outlook.es>
2025-10-31 23:34:06 +0000

    Greeting component added and Implement basic Parent-Child comms via Signals

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [GreetingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   home = 'this data is coming from home component using binding interpolation in Angular 16-.';
+  homeMessage = signal('Hello from Home Component using Angular signals! (Parent to Child Communication), so Hi Greeting Component!');
 }
