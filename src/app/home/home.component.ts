cb09788f0e39ffea55d5c76713cc9bc937c778b2
Kevin Montaño <kevindmm@outlook.es>
2025-11-01 14:44:43 +0000

    Event binding using listener, keyUpHandler

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,14 @@ import { GreetingComponent } from '../components/greeting/greeting.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  //Binding interpolation in Angular
   home = 'this data is coming from home component using binding interpolation in Angular 16-.';
+
+  //Signal for Parent to Child Communication
   homeMessage = signal('Hello from Home Component using Angular signals! (Parent to Child Communication), so Hi Greeting Component!');
+
+  //Event Listener keyuphandler
+  keyUpHandler(event: KeyboardEvent) {
+    console.log( `User pressed the "${event.key}" key`);
+  }
 }
